d711e05decef855fc27e5a6db1b2a47da7593220
Jeremia hays Jarfi <pacelinux@gmail.com>
2020-05-27 04:15:53 +0900

    first commit

--- src/main/java/com/jjarfi/github/ntt/NttApplication.java
+++ src/main/java/com/jjarfi/github/ntt/NttApplication.java
@@ -1,13 +1,21 @@
 package com.jjarfi.github.ntt;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @SpringBootApplication
 public class NttApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NttApplication.class, args);
+	}
+	@Bean
+	public LayoutDialect layoutDialect() {
+		return new LayoutDialect();
 	}
 
 }
